d2d22e27fd733b8a399e20778d73c458e16bf1c2
Codie Nichols <cbn8191@uncw.edu>
2019-08-19 11:33:04 -0400

    Fixes after testing app on real phone

--- app/src/main/java/com/example/lenovo/gardenclub/LoginProcess.java
+++ app/src/main/java/com/example/lenovo/gardenclub/LoginProcess.java
@@ -12,6 +12,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
