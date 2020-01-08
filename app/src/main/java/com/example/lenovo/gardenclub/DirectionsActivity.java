3225e5c7b12dabf3ae929127311225703f1d6633
Codie Nichols <cbn8191@uncw.edu>
2020-01-07 21:52:42 -0500

    Fixed the garden pages

--- app/src/main/java/com/example/lenovo/gardenclub/DirectionsActivity.java
+++ app/src/main/java/com/example/lenovo/gardenclub/DirectionsActivity.java
@@ -13,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.constraintlayout.widget.Guideline;
 
 public class DirectionsActivity extends AppCompatActivity {
     private Intent i;
@@ -26,9 +27,13 @@ public class DirectionsActivity extends AppCompatActivity {
         Button btnHome = findViewById(R.id.homeButton12);
         TextView titleTV = findViewById(R.id.titleTV);
         TextView descTV = findViewById(R.id.descriptionTV);
+        TextView dLabel = findViewById(R.id.dLabel);
         ImageView garden = findViewById(R.id.gardenIV);
-        Button btnGetDir = findViewById(R.id.getDirectionsButton);
+        Button btnF = findViewById(R.id.fButton);
+        Button btnS = findViewById(R.id.sButton);
         Button btnBack = findViewById(R.id.backButton);
+        Guideline g8 = findViewById(R.id.guideline8);
+        Guideline g10 = findViewById(R.id.guideline10);
         ConstraintSet cs = new ConstraintSet();
         ConstraintLayout cL = findViewById(R.id.cL);
 
@@ -36,7 +41,9 @@ public class DirectionsActivity extends AppCompatActivity {
 
         titleTV.setTypeface(arial);
         descTV.setTypeface(arial);
-        btnGetDir.setTypeface(arial);
+        dLabel.setTypeface(arial);
+        btnF.setTypeface(arial);
+        btnS.setTypeface(arial);
         btnBack.setTypeface(arial);
 
         btnHome.setOnClickListener(new View.OnClickListener() {
@@ -57,9 +64,14 @@ public class DirectionsActivity extends AppCompatActivity {
 
         switch(marker) {
             case "RC":
+                titleTV.setText(R.string.about);
                 descTV.setText(R.string.rc_marker);
                 garden.setImageResource(R.drawable.ribboncutting);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.83f);
+                g10.setGuidelinePercent(0.59f);
+                btnF.setText(R.string.get_directions_to_ribbon_cutting_opens_google_maps);
+                btnF.setTextSize(18.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         uri = Uri.parse("google.navigation:q=314+Pine+Grove+Drive+Wilmington,+North+Carolina&mode=d");
@@ -74,13 +86,38 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about1);
                 descTV.setText(R.string.marker_1);
                 garden.setImageResource(R.drawable.garden1);
+                g8.setGuidelinePercent(0.78f);
+                g10.setGuidelinePercent(0.55f);
                 cs.clone(cL);
-
-                btnGetDir.setText(R.string.get_directions_to_marker_1);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                cs.clear(btnF.getId(), ConstraintSet.END);
+                cs.clear(btnF.getId(), ConstraintSet.TOP);
+                cs.clear(btnF.getId(), ConstraintSet.START);
+                cs.setVisibility(dLabel.getId(), ConstraintSet.VISIBLE);
+                cs.setVisibility(btnS.getId(), ConstraintSet.VISIBLE);
+                cs.connect(btnF.getId(), ConstraintSet.TOP, dLabel.getId(), ConstraintSet.BOTTOM, 8);
+                cs.connect(btnF.getId(), ConstraintSet.END, R.id.guideline12, ConstraintSet.START, 8);
+                cs.connect(btnF.getId(), ConstraintSet.START, cL.getId(), ConstraintSet.START, 8);
+                cs.applyTo(cL);
+                btnF.setText(R.string.friday);
+                dLabel.setText(R.string.get_directions_to_marker_1);
+                btnS.setText(R.string.saturday_sunday);
+                btnF.setTextSize(22.0f);
+                dLabel.setTextSize(22.0f);
+                btnS.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        uri = Uri.parse("google.navigation:q=6451+Quail+Run+Road+Wilmington,+North+Carolina&mode=d");
+                        uri = Uri.parse("google.navigation:q=314+Pine+Grove+Drive+Wilmington,+North+Carolina&mode=d");
+                        i = new Intent(Intent.ACTION_VIEW, uri);
+                        i.setPackage("com.google.android.apps.maps");
+                        if(i.resolveActivity(getPackageManager()) != null)
+                            startActivity(i);
+                    }
+                });
+                btnS.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        uri = Uri.parse("google.navigation:q=6211+Greenville+Loop+Road+Wilmington,+North+Carolina&mode=d");
                         i = new Intent(Intent.ACTION_VIEW, uri);
                         i.setPackage("com.google.android.apps.maps");
                         if(i.resolveActivity(getPackageManager()) != null)
@@ -92,11 +129,38 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about2);
                 descTV.setText(R.string.marker_2);
                 garden.setImageResource(R.drawable.garden2);
-                btnGetDir.setText(R.string.get_directions_to_marker_2);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.78f);
+                g10.setGuidelinePercent(0.55f);
+                cs.clone(cL);
+                cs.clear(btnF.getId(), ConstraintSet.END);
+                cs.clear(btnF.getId(), ConstraintSet.TOP);
+                cs.clear(btnF.getId(), ConstraintSet.START);
+                cs.setVisibility(dLabel.getId(), ConstraintSet.VISIBLE);
+                cs.setVisibility(btnS.getId(), ConstraintSet.VISIBLE);
+                cs.connect(btnF.getId(), ConstraintSet.TOP, dLabel.getId(), ConstraintSet.BOTTOM, 8);
+                cs.connect(btnF.getId(), ConstraintSet.END, R.id.guideline12, ConstraintSet.START, 8);
+                cs.connect(btnF.getId(), ConstraintSet.START, cL.getId(), ConstraintSet.START, 8);
+                cs.applyTo(cL);
+                btnF.setText(R.string.friday);
+                dLabel.setText(getString(R.string.get_directions_to_marker_2));
+                btnS.setText(R.string.saturday_sunday);
+                btnF.setTextSize(22.0f);
+                dLabel.setTextSize(22.0f);
+                btnS.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        uri = Uri.parse("google.navigation:q=6315+Old+Military+Road+Wilmington,+North+Carolina&mode=d");
+                        uri = Uri.parse("google.navigation:q=314+Pine+Grove+Drive+Wilmington,+North+Carolina&mode=d");
+                        i = new Intent(Intent.ACTION_VIEW, uri);
+                        i.setPackage("com.google.android.apps.maps");
+                        if(i.resolveActivity(getPackageManager()) != null)
+                            startActivity(i);
+                    }
+                });
+                btnS.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        uri = Uri.parse("google.navigation:q=6211+Greenville+Loop+Road+Wilmington,+North+Carolina&mode=d");
                         i = new Intent(Intent.ACTION_VIEW, uri);
                         i.setPackage("com.google.android.apps.maps");
                         if(i.resolveActivity(getPackageManager()) != null)
@@ -108,8 +172,11 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about3);
                 descTV.setText(R.string.marker_3);
                 garden.setImageResource(R.drawable.garden3);
-                btnGetDir.setText(R.string.get_directions_to_marker_3);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.84f);
+                g10.setGuidelinePercent(0.60f);
+                btnF.setText(R.string.get_directions_to_marker_3);
+                btnF.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         uri = Uri.parse("google.navigation:q=300+Airlie+Road+Wilmington,+North+Carolina&mode=d");
@@ -124,11 +191,14 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about4);
                 descTV.setText(R.string.marker_4);
                 garden.setImageResource(R.drawable.garden4);
-                btnGetDir.setText(R.string.get_directions_to_marker_4);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.84f);
+                g10.setGuidelinePercent(0.60f);
+                btnF.setText(R.string.get_directions_to_marker_4);
+                btnF.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        uri = Uri.parse("google.navigation:q=Seawater+Lane+Wilmington,+North+Carolina&mode=d");
+                        uri = Uri.parse("google.navigation:q=Seawater+Lane+Wrightsville+Beach,+North+Carolina&mode=d");
                         i = new Intent(Intent.ACTION_VIEW, uri);
                         i.setPackage("com.google.android.apps.maps");
                         if(i.resolveActivity(getPackageManager()) != null)
@@ -140,8 +210,11 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about5);
                 descTV.setText(R.string.marker_5);
                 garden.setImageResource(R.drawable.garden5);
-                btnGetDir.setText(R.string.get_directions_to_marker_5);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.84f);
+                g10.setGuidelinePercent(0.60f);
+                btnF.setText(R.string.get_directions_to_marker_5);
+                btnF.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         uri = Uri.parse("google.navigation:q=6309+Pintail+Court+Wilmington,+North+Carolina&mode=d");
@@ -156,8 +229,11 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about6);
                 descTV.setText(R.string.marker_6);
                 garden.setImageResource(R.drawable.garden6);
-                btnGetDir.setText(R.string.get_directions_to_marker_6);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.84f);
+                g10.setGuidelinePercent(0.60f);
+                btnF.setText(R.string.get_directions_to_marker_6);
+                btnF.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         uri = Uri.parse("google.navigation:q=707+Colonial+Drive+Wilmington,+North+Carolina&mode=d");
@@ -172,8 +248,11 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about7);
                 descTV.setText(R.string.marker_7);
                 garden.setImageResource(R.drawable.garden7);
-                btnGetDir.setText(R.string.get_directions_to_marker_7);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.84f);
+                g10.setGuidelinePercent(0.60f);
+                btnF.setText(R.string.get_directions_to_marker_7);
+                btnF.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         uri = Uri.parse("google.navigation:q=314+Colonial+Drive+Wilmington,+North+Carolina&mode=d");
@@ -188,8 +267,11 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about8);
                 descTV.setText(R.string.marker_8);
                 garden.setImageResource(R.drawable.garden8);
-                btnGetDir.setText(R.string.get_directions_to_marker_8);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.84f);
+                g10.setGuidelinePercent(0.60f);
+                btnF.setText(R.string.get_directions_to_marker_8);
+                btnF.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         uri = Uri.parse("google.navigation:q=1710+Chestnut+Street+Wilmington,+North+Carolina&mode=d");
@@ -204,8 +286,11 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about9);
                 descTV.setText(R.string.marker_9);
                 garden.setImageResource(R.drawable.garden9);
-                btnGetDir.setText(R.string.get_directions_to_marker_9);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.84f);
+                g10.setGuidelinePercent(0.60f);
+                btnF.setText(R.string.get_directions_to_marker_9);
+                btnF.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         uri = Uri.parse("google.navigation:q=1708+Chestnut+Street+Wilmington,+North+Carolina&mode=d");
@@ -220,8 +305,11 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about10);
                 descTV.setText(R.string.marker_10);
                 garden.setImageResource(R.drawable.garden10);
-                btnGetDir.setText(R.string.get_directions_to_marker_10);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.84f);
+                g10.setGuidelinePercent(0.60f);
+                btnF.setText(R.string.get_directions_to_marker_10);
+                btnF.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         uri = Uri.parse("google.navigation:q=1711+Princess+Street+Wilmington,+North+Carolina&mode=d");
@@ -236,8 +324,11 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about11);
                 descTV.setText(R.string.marker_11);
                 garden.setImageResource(R.drawable.garden11a);
-                btnGetDir.setText(R.string.get_directions_to_marker_11);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.84f);
+                g10.setGuidelinePercent(0.60f);
+                btnF.setText(R.string.get_directions_to_marker_11);
+                btnF.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         uri = Uri.parse("google.navigation:q=512+Grace+Street+Wilmington,+North+Carolina&mode=d");
@@ -252,8 +343,11 @@ public class DirectionsActivity extends AppCompatActivity {
                 titleTV.setText(R.string.about12);
                 descTV.setText(R.string.marker_12);
                 garden.setImageResource(R.drawable.garden12);
-                btnGetDir.setText(R.string.get_directions_to_marker_12);
-                btnGetDir.setOnClickListener(new View.OnClickListener() {
+                g8.setGuidelinePercent(0.84f);
+                g10.setGuidelinePercent(0.60f);
+                btnF.setText(R.string.get_directions_to_marker_12);
+                btnF.setTextSize(22.0f);
+                btnF.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         uri = Uri.parse("google.navigation:q=5024+Split+Rail+Drive+Wilmington,+North+Carolina&mode=d");
